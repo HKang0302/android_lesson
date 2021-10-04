3b2f96b3c09fa6ec7379932c715070618a786fd3
HKang0302 <heejin444@gmail.com>
2021-10-05 00:30:18 +0900

    2021.10.04 android UI DONE

--- app/src/main/java/com/example/androidlesson/MainActivity.kt
+++ app/src/main/java/com/example/androidlesson/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.androidlesson
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.scroll_view_12)
+        setContentView(R.layout.final_ui)
     }
 }
