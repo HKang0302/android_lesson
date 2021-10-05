8752617205817eecc86b5876bccdfb76427a0bea
HKang0302 <heejin444@gmail.com>
2021-10-05 23:09:05 +0900

    2021/10/05 Fragment part done

--- app/src/main/java/com/example/androidlesson/FragmentActivity.kt
+++ app/src/main/java/com/example/androidlesson/FragmentActivity.kt
@@ -3,12 +3,49 @@ package com.example.androidlesson
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 
-class FragmentActivity : AppCompatActivity() {
+class FragmentActivity : AppCompatActivity(), FragmentOne.OnDataPassListener {
+
+    override fun onDataPass(data: String?) {
+        Log.d("pass", ""+data)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fragment)
         Log.d("life_cycle", "onCreate")
+
+        var button: Button = findViewById(R.id.frag_button)
+        var button2: Button = findViewById(R.id.frag_button2)
+
+        // fragment를 동적으로 작동하는 방법
+        val frag1 : FragmentOne = FragmentOne()
+        val fragmentManager : FragmentManager = supportFragmentManager
+
+        // fragement에 데이터를 넣는 방법
+        val bundle: Bundle = Bundle()
+        bundle.putString("hello", "hello")
+        frag1.arguments = bundle
+
+        button.setOnClickListener {
+            // Transaction (작업 단위)
+            // fragment replace/add
+            val fragTransaction = fragmentManager.beginTransaction()
+            fragTransaction.replace(R.id.container, frag1)
+            fragTransaction.commit()
+        }
+
+        button2.setOnClickListener {
+            // fragment remove/detach 하는 방법
+            // remove: 없앤 후 다시 나타내게 하고 싶을 때
+            // detach: 없앤 후 다시 나타내지 않을 때
+            val fragTransaction = fragmentManager.beginTransaction()
+            fragTransaction.remove(frag1)
+            fragTransaction.commit()
+        }
     }
 
     override fun onStart() {
--- app/src/main/java/com/example/androidlesson/FragmentOne.kt
+++ app/src/main/java/com/example/androidlesson/FragmentOne.kt
@@ -6,16 +6,26 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 
 class FragmentOne : Fragment() {
+
+    // 리스너 구현
+    interface OnDataPassListener{
+        fun onDataPass(data: String?)
+    }
+
+    lateinit var dataPassListener: OnDataPassListener
+
     override fun onAttach(context: Context) {
         Log.d("life_cycle", "    override fun onAttach(context: Context) {\n")
         super.onAttach(context)
+        dataPassListener = context as OnDataPassListener
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        Log.d("life_cycle", "onCreate")
+        Log.d("life_cycle", "F onCreate")
         super.onCreate(savedInstanceState)
     }
 
@@ -24,49 +34,61 @@ class FragmentOne : Fragment() {
         container: ViewGroup?,      // 부모뷰
         savedInstanceState: Bundle?
     ): View? {
-        Log.d("life_cycle", "onCreateView")
+        Log.d("life_cycle", "F onCreateView")
 
         // fragment가 인터페이스를 처음으로 그릴 때 호출됨
         return inflater.inflate(R.layout.fragment_one, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        Log.d("life_cycle", "onViewCreated")
+        Log.d("life_cycle", "F onViewCreated")
         super.onViewCreated(view, savedInstanceState)
+        val pass: Button = view.findViewById(R.id.pass)
+
+        // Activity의 onCreate에서 하는 작업을 onViewCreated에서 진행
+        pass.setOnClickListener {
+            dataPassListener.onDataPass("Good Bye")
+        }
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
-        Log.d("life_cycle", "onActivityCreated")
+        Log.d("life_cycle", "F onActivityCreated")
+        val data = arguments?.getString("hello")
+        if (data != null) {
+            Log.d("data", data)
+        }
+
+
         super.onActivityCreated(savedInstanceState)
     }
 
     override fun onStart() {
-        Log.d("life_cycle", "onStart")
+        Log.d("life_cycle", "F onStart")
         super.onStart()
     }
 
     override fun onResume() {
-        Log.d("life_cycle", "onResume")
+        Log.d("life_cycle", "F onResume")
         super.onResume()
     }
 
     override fun onPause() {
-        Log.d("life_cycle", "onPause")
+        Log.d("life_cycle", "F onPause")
         super.onPause()
     }
 
     override fun onStop() {
-        Log.d("life_cycle", "onStop")
+        Log.d("life_cycle", "F onStop")
         super.onStop()
     }
 
     override fun onDestroyView() {
-        Log.d("life_cycle", "onDestroyView")
+        Log.d("life_cycle", "F onDestroyView")
         super.onDestroyView()
     }
 
     override fun onDetach() {
-        Log.d("life_cycle", "onDetach")
+        Log.d("life_cycle", "F onDetach")
         super.onDetach()
     }
 
